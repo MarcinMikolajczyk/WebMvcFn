83cd6ab2940ebcc5c302645c9563276e12791361
Marcin Mikołajczyk <Marcin.Mikolajczyk902@outlook.com>
2020-10-03 21:49:52 +0200

    first init

---
+++ src/main/java/com/example/WebMvcFn/WebMvcFnApplication.java
@@ -0,0 +1,78 @@
+package com.example.WebMvcFn;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.function.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.springframework.http.MediaType.*;
+import static org.springframework.web.servlet.function.RouterFunctions.*;
+import static org.springframework.web.servlet.function.ServerResponse.*;
+
+@SpringBootApplication
+public class WebMvcFnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcFnApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routes (PetService petService){
+		return route()
+				.GET("/pets", serverRequest -> ok().contentType(APPLICATION_JSON).body(petService.all()))
+				.GET("/pet/{id}", serverRequest ->
+						ok().contentType(APPLICATION_JSON)
+								.body(petService.getById(Long.parseLong(serverRequest.pathVariable("id")))))
+				.POST("/pet", r -> {
+					Pet result = petService.save(new Pet(null, r.body(Pet.class).getName()));
+					URI uri = URI.create("/pet/" + result.getId());
+					return ServerResponse.created(uri).body(result);
+				})
+				.build();
+	}
+}
+
+@Service
+class PetService{
+
+	private final AtomicLong counter = new AtomicLong(3L);
+
+	private final List<Pet> pets = List.of(
+			new Pet(1L, "Jeff"),
+			new Pet(2L, "Luke"),
+			new Pet(3L, "Mia")
+	);
+
+	List<Pet> all(){
+		return pets;
+	}
+
+	Pet getById(Long id){
+		return pets.stream()
+				.filter(pet -> pet.getId().equals(id))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException("No " + Pet.class.getName() + " with that ID found!"));
+	}
+
+	Pet save(Pet p){
+		Pet pet = new Pet(counter.incrementAndGet(), p.getName());
+		this.pets.add(pet);
+		return pet;
+	}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Pet{
+	private Long id;
+	private String name;
+}
---
+++ src/test/java/com/example/WebMvcFn/WebMvcFnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebMvcFn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcFnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
